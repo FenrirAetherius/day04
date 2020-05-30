8935fa2f330c0e6166b5eda99509b29c20c8836a
fenriraetherius <blueyesmax69@gmail.com>
2020-05-30 22:56:11 +0200

    last commit

--- ex02/ProportionBySport.py
+++ ex02/ProportionBySport.py
@@ -3,12 +3,14 @@ def proportionBySport(df, yr, sport, gender):
     df = df.loc[yr]
     df.set_index("Sex", inplace=True)
     df = df.loc[gender]
-    tot = df.shape[0]
+    df_tot = df
+    df_tot.drop_duplicates(subset="Name", keep=False, inplace=True)
+    tot = df_tot.shape[0]
     df.set_index("Sport", inplace=True)
     df = df.loc[sport]
     print(df.shape[0] / tot * 100)
 
-from FileLoader import FileLoader
-loader = FileLoader()
-data = loader.load('athlete_events.csv')
-proportionBySport(data, 2004, 'Tennis', 'F')
+# from FileLoader import FileLoader
+# loader = FileLoader()
+# data = loader.load('../data/athlete_events.csv')
+# proportionBySport(data, 2004, 'Tennis', 'F')
